204ae35e5d03caa586c8ed156e3da7ddcd3a34fb
Archana Nair <archana.nair@bakuun.com>
2026-06-11 21:38:29 +0530

    feat: add production build assets and configure initial Laravel project views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Research Report System</title>
     <link rel="icon" type="image/png" href="/favicon.png?v=3.0">
     <script src="https://cdn.ckeditor.com/4.22.1/full-all/ckeditor.js"></script>
